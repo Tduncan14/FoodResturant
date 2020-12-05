02e1c5b4b98a9b2ad73074a756b8aaf7f6fb6bc2
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-12-05 03:52:06 -0600

    keep moving forward

---
+++ images/script.js
@@ -0,0 +1,13 @@
+
+const icons = document.querySelectorAll(
+    '.section-1-icons i'
+)
+
+setInterval(
+() =>{
+
+    const icon = document.querySelector('.section-1-icons .change')
+
+
+
+},4000)
